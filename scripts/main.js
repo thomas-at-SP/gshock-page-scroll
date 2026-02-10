2ec6986db81f130cf5d51472ca4c4d35da0151e6
thomas-at-mages-edu-sg <thomas@mages.edu.sg>
2026-02-10 09:23:03 +0800

    Can import threepipe

--- scripts/main.js
+++ scripts/main.js
@@ -1,68 +1,87 @@
-import {
-  ViewerApp,
-  AssetManagerPlugin,
-  addBasePlugins,
-  ScrollableCameraViewPlugin,
-  VariationConfiguratorPlugin,
-  FrameFadePlugin,
-  LoadingScreenPlugin,
-  PickingPlugin,
-  TweakpaneUiPlugin,
-  MaterialConfiguratorPlugin,
+// import {
+//   ViewerApp,
+//   AssetManagerPlugin,
+//   addBasePlugins,
+//   ScrollableCameraViewPlugin,
+//   VariationConfiguratorPlugin,
+//   FrameFadePlugin,
+//   LoadingScreenPlugin,
+//   PickingPlugin,
+//   TweakpaneUiPlugin,
+//   MaterialConfiguratorPlugin,
 
-  // Import THREE.js internals
-  Color,
-	Texture,
-  Vector3
-} from 'webgi';
+//   // Import THREE.js internals
+//   Color,
+// 	Texture,
+//   Vector3
+// } from 'webgi';
+import {
+  ContactShadowGroundPlugin,
+  LoadingScreenPlugin,
+  ProgressivePlugin,
+  SSAAPlugin,
+  ScrollAnimationPlugin,
+  ThreeViewer
+} from 'threepipe';
 
 async function setupViewer() {
-  const viewer = new ViewerApp({
-      canvas: document.getElementById('web-canvas'),
+//   const viewer = new ViewerApp({
+//       canvas: document.getElementById('web-canvas'),
+//   });
+  const viewer = new ThreeViewer({
+    canvas: document.getElementById('web-canvas'),
+    msaa: false,
+    renderScale: "auto",
+    tonemap: true,
+    plugins: [
+      LoadingScreenPlugin,
+      ProgressivePlugin, SSAAPlugin,
+      ContactShadowGroundPlugin
+    ]
   });
 
-  await addBasePlugins(viewer);
-  await viewer.addPlugin(ScrollableCameraViewPlugin);
+//   await addBasePlugins(viewer);
+//   await viewer.addPlugin(ScrollableCameraViewPlugin);
 
-  // const manager = await viewer.getPlugin(AssetManagerPlugin);
-  // This must be called after adding any plugin that changes the render pipeline.
-	viewer.renderer.refreshPipeline();
+//   // const manager = await viewer.getPlugin(AssetManagerPlugin);
+//   // This must be called after adding any plugin that changes the render pipeline.
+// 	viewer.renderer.refreshPipeline();
 
-  // Load an environment map if not set in the glb file
-  await viewer.setEnvironmentMap("./assets/autumn forest.hdr");
+//   // Load an environment map if not set in the glb file
+   await viewer.setEnvironmentMap("./assets/autumn forest.hdr");
 
-  // await manager.addFromPath("./assets/casio watch.glb");
-  await viewer.load("./assets/casio g-shock watch v2.glb");
+//   // await manager.addFromPath("./assets/casio watch.glb");
+   await viewer.load("./assets/casio g-shock watch v2.glb");
 
-  // let scrollSection = document.getElementById("scrollSection");
-  // await viewer.getPlugin(new ScrollableCameraViewPlugin(scrollSection));
+//   // let scrollSection = document.getElementById("scrollSection");
+//   // await viewer.getPlugin(new ScrollableCameraViewPlugin(scrollSection));
 }
 
 setupViewer();
 
-window.onscroll = function() {
-  if(document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-      document.querySelector('.back-to-top').style.display = 'block';
-  } else {
-      document.querySelector('.back-to-top').style.display = 'none';
-  }
-};
+// window.onscroll = function() {
+//   if(document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+//       document.querySelector('.back-to-top').style.display = 'block';
+//   } else {
+//       document.querySelector('.back-to-top').style.display = 'none';
+//   }
+// };
 
-let scrollSpeed = 1.0;
+// let scrollSpeed = 1.0;
 
-// Add an event listener for the 'wheel' event
-document.addEventListener('wheel', function(event) {
-  // Prevent default scrolling behavior
-  event.preventDefault();
+// // Add an event listener for the 'wheel' event
+// document.addEventListener('wheel', function(event) {
+//   // Prevent default scrolling behavior
+//   event.preventDefault();
 
-  // Calculate the new scroll position
-  let delta = event.deltaY;
-  let scrollPosition = window.scrollY + (delta * scrollSpeed);
+//   // Calculate the new scroll position
+//   let delta = event.deltaY;
+//   let scrollPosition = window.scrollY + (delta * scrollSpeed);
 
-  // Set the new scroll position
-  window.scrollTo({
-    top: scrollPosition,
-    behavior: 'smooth'
-  });
-},
-{ passive: false });
+//   // Set the new scroll position
+//   window.scrollTo({
+//     top: scrollPosition,
+//     behavior: 'smooth'
+//   });
+// },
+// { passive: false });
